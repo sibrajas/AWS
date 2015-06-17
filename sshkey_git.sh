03923ee5bc087a69ba801045217b56e8fc4319ff
Sibi Rajasekaran <log2sibi@gmail.com>
2015-06-17 07:14:28 +0000

    ip and utilities

--- sshkey_git.sh
+++ sshkey_git.sh
@@ -3,3 +3,5 @@ git config --global user.email ""
 sh-keygen -t rsa -b 4096 -C "email"
 cat ~/.ssh/id_rsa.pub and copy to ssh keys to github
 ssh -T git@github.com
+git clone git@github.com:sibrajas/AWS.git
+git remote set-url origin git@github.com:sibrajas/AWS.git
